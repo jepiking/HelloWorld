40614f447dccd0237eda53651285b86393753e28
Jepiking <92075130+jepiking@users.noreply.github.com>
2021-10-22 16:58:59 +0300

    Add files via upload

---
+++ HelloWorld.js
@@ -0,0 +1,16 @@
+var helloWorld = {
+    "H": "H",
+    "E": "e",
+    "L": "l",
+    "L1": "l",
+    "O": "o",
+    "-": " ",
+    "W": "W",
+    "O1": "o",
+    "R": "r",
+    "L2": "l",
+    "D": "d",
+    "!": "!"
+}
+var helloWorld = helloWorld.H + helloWorld.E + helloWorld.L + helloWorld.L1 + helloWorld.O + helloWorld["-"] + helloWorld.W + helloWorld.O1 + helloWorld.R + helloWorld.L + helloWorld.D + helloWorld["!"];
+console.log(helloWorld)
